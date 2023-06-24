942e2df7c7a948c16934cca92faa391ecb461924
IBugagashI <popykin.alek@yandex.ru>
2023-06-24 23:00:23 +0300

    task
    заметки

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from tkinter import *
-from tkinter import messagebox
-from tkinter.filedialog import askopenfile, asksaveasfile
-
+                        # Заметки на Python
+from tkinter import *   # tkinter - это кроссплатформенная библиотека для разработки графического интерфейса на языке Python 
+from tkinter import messagebox # messagebox - это всплывающее окно, которое появляется на экране для того, чтобы дать вам простую текстовую информацию
+from tkinter.filedialog import askopenfile, asksaveasfile # Модуль filedialog предоставляет функциональность файловых диалоговых окон, которые позволяют выбрать файл или каталог для различных задач
+                               # askopenfile(): открывает диалоговое окно для выбора файла и возвращает выбранный файл. Если файл не выбран, возвращается None
+                               # asksaveasfile(): открывает диалоговое окно для сохранения файла и возвращает сохраненный файл. Если файл не выбран, возвращается None
 file_name = NONE
 
 def new_file():
     global file_name
     file_name = "Без названия"
-    text.delete('1.0', END)
+    text.delete('1.0', END) # Удаление всего, что было в заметке. Всё с чистого листа
 
 def save_as():
     out = asksaveasfile(mode='w', defaultextension='.txt')
@@ -26,15 +28,15 @@ def open_file():
     text.delete('1.0', END)
     text.insert('1.0', data)
 
-root = Tk()
-root.title("Заметки")
-root.geometry("300x300")
+root = Tk() # корневой экземпляр Tk
+root.title("Заметки")    # .title - титульное название 
+root.geometry("300x300") # .geometry - определяет размер открываемого стикера
 
-text = Text(root, width=300, height=300)
+text = Text(root, width=300, height=300) # размер поля для ввода текста
 text.pack()
 
-menu_bur = Menu(root)
-file_menu = Menu(menu_bur)
+menu_bur = Menu(root) 
+file_menu = Menu(menu_bur) # переменная, для наполнения Меню функциями действия
 
 file_menu.add_command(label="Новый", command=new_file)
 file_menu.add_command(label="Открыть", command=open_file)
